f790b49862ecbb97781cf369eea3185874a38f06
Mayank Sharma <mayankofficial167@gmail.com>
2025-01-06 10:43:51 +0530

    404 error page updated

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -37,6 +37,10 @@ const router = createBrowserRouter([
       {
         path: "/signup",
         element: <Signup />
+      },
+      {
+        path: "*",
+        element: <Home />,
       }
 
     ],
